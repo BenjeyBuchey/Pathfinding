9717b0b1e565002ad587e5e58ccded3b91ea9170
BenjeyBuchey <ch0b0@gmx.at>
2019-01-09 19:14:00 +0100

    Multiple maps
    fixed size, position + border

--- Pathfinding2D/Assets/Scripts/Map/GameFieldScript.cs
+++ Pathfinding2D/Assets/Scripts/Map/GameFieldScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,42 +8,47 @@ public class GameFieldScript : MonoBehaviour {
 
 	private float initWidth, initHeight;
 	public GameObject _map;
+	private int mapSizeDivider = 1, border = 10;
 	// Use this for initialization
 	void Start () {
-		//initWidth = this.gameObject.GetComponent<RectTransform>().rect.width;
-		//initHeight = this.gameObject.GetComponent<RectTransform>().rect.height;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		//float width = this.gameObject.GetComponent<RectTransform>().rect.width;
-
-		//Debug.Log("INIT WIDTH: " + initWidth);
-		//Debug.Log("Current WIDTH: " + width);
-		
-		//int newCols = 0, rows = 0;
-
-		//int currentCols = (int)(initWidth / width);
-		//Debug.Log("Current #Columns: " + currentCols);
-		//int numMaps = GameObject.FindGameObjectsWithTag("Map").Length;
-		///Debug.Log("NUmber of Maps: " + numMaps);
-
-		//if (numMaps > currentCols * currentCols)
-		//{
-		//	newCols = currentCols + 1;
-		//	Debug.Log("New #Columns: " + newCols);
-
-		//	Vector2 newSize = new Vector2(width / newCols, width / newCols);
-		//	this.gameObject.GetComponent<GridLayoutGroup>().cellSize = newSize;
-		//}
+		initWidth = this.gameObject.GetComponent<RectTransform>().rect.width;
+		initHeight = this.gameObject.GetComponent<RectTransform>().rect.height;
 	}
 
 	public void SpawnNewMap()
 	{
-		// TODO: check total number of maps, resize if needed, set positions, small border between maps
+		int newMapCount = GameObject.FindGameObjectsWithTag("Map").Length+1;
+		Debug.Log("CURRENT #Maps: " + newMapCount);
+
+		if (newMapCount > mapSizeDivider * mapSizeDivider) mapSizeDivider++;
+
 		GameObject map = Instantiate(_map, gameObject.transform);
-		RectTransform rt = map.GetComponent<RectTransform>();
-		rt.sizeDelta = new Vector2(325, 325);
+		ResizeAndSetPositions();
+	}
+
+	private void ResizeAndSetPositions()
+	{
+		// + in x axis, - in y axis
+		GameObject[] maps = GameObject.FindGameObjectsWithTag("Map");
+
+		int mapIndex = 0;
+		// y = rows, x = columns
+		for(int y = 0; y < mapSizeDivider; y++)
+		{
+			for(int x = 0; x < mapSizeDivider; x++)
+			{
+				GameObject map = maps[mapIndex];
+				// size
+				RectTransform rt = map.GetComponent<RectTransform>();
+				rt.sizeDelta = new Vector2((initWidth - border * (mapSizeDivider-1))/(mapSizeDivider), (initWidth - border * (mapSizeDivider - 1)) / (mapSizeDivider));
+
+				// position
+				rt.localPosition = new Vector3(x * (rt.sizeDelta.x + border), -y * (rt.sizeDelta.x + border), 0.0f);
+
+				mapIndex++;
+				if (mapIndex >= maps.Length) break;
+			}
+			if (mapIndex >= maps.Length) break;
+		}
 	}
 }
